5ac4e2fe9c0a4950a4a0337b06e2099b83cfda5b
Daniel Buttig <daniel.buttig@gmail.com>
2025-09-23 23:47:12 -0500

    Lesson 6 - Arrow Functions
    Wrote js using traditional function syntax, then replaced with the arrow
    function syntax. Wrote functions that accepted no parameters, single
    parameters (demonstrating the lack of need of parenthesis), and examples
    using single and multi-line function definitions.

--- fullstack/index.js
+++ fullstack/index.js
@@ -1,6 +1,25 @@
-function welcome(user='Mystery person', message='Goodday'){
-  alert(`Hello ${user}, ${message}`);
+//function greeting(message) {
+  //return alert(`${message}, everyone!`)
+//}
+
+let noParamFunction = () => alert('This is only a test.');
+
+noParamFunction();
+
+let greeting = message => alert(`${message}, everyone!`);
+
+greeting('Good morning');
+
+let createBlog = (title, body) => {
+  if (!title) {
+    throw new Error('A title is required');
+  }
+
+  if (!body) {
+    throw new Error('Body cannot be empty');
+  }
+
+  return alert(`${title} - ${body}`);
 }
 
-//welcome('Daniel', 'Good morning');
-welcome();
+createBlog('My title', 'Blog body');
