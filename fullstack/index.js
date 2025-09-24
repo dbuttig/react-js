902bb41e50b1724de221680d4b9bf8cc2331cffc
Daniel Buttig <daniel.buttig@gmail.com>
2025-09-23 23:05:20 -0500

    Lesson 5 - Default Parameters
    Redesigned index.js to use a function. This function originally would
    alert the user on page load using hard-coded strings provided in the
    function call. Later we practiced setting up default values in the
    function definition.

--- fullstack/index.js
+++ fullstack/index.js
@@ -1,11 +1,6 @@
-let fname= 'Daniel';
-let lname = 'Buttig';
-let age = prompt("Guess Daniel's age..");
+function welcome(user='Mystery person', message='Goodday'){
+  alert(`Hello ${user}, ${message}`);
+}
 
-//  old way
-//let result = fname +  ' ' + lname + ' is ' + age + ' years old';
-//alert(result);
-
-// using template string
-let result = `${fname} ${lname} is ${age} years old!`;
-alert(result);
+//welcome('Daniel', 'Good morning');
+welcome();
